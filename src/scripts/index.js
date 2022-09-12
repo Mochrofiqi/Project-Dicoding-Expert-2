fc55b746ea9db734e9c335a1418263fae4888079
Moch. Rofiqi <mrfiqi323@gmail.com>
2022-09-12 13:25:05 +0700

    init

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime'; 
+import '../styles/main.css'
+import '../styles/responsive.css'
+import '../styles/navbar.css'
+import './navbar'
+import main from './main.js';
+
+document.addEventListener("DOMContentLoaded", main);
+
